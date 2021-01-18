b18ae903b4332d0f93085d233f930f9ddfbf5bb8
TymanWasTaken <TymanWasTaken@users.noreply.github.com>
2021-01-18 14:02:04 -0700

    more stuff

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import express from 'express'
-const app = express() 
+import { stat } from 'fs'
+import { promisify } from 'util'
+
+const app = express()
 const port = 8738
 const _dirname = __dirname.replace('\\dist', '')
+const statP = promisify(stat)
 
 app.use(express.static(_dirname + '/static'))
 app.get('/', ( req, res ) => {
 	res.sendFile(_dirname + '/static/index.html')
+})
+
+app.get('*', async (req, res) => {
+	try {
+		await statP(_dirname + '/static' + req.path + '.html')
+		res.sendFile(_dirname + '/static' + req.path + '.html')
+	} catch {
+		res.sendStatus(404)
+	}
+
 })
 // start the Express server
 app.listen(port, () => {
