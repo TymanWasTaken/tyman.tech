ebab5ffb27b63c747d34a6f7e12b3a557ecc2794
Tyman <tymanwastaken@users.noreply>
2021-01-18 13:38:45 -0700

    stuff

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import express from 'express'
-const app = express(); 
-const port = 8738;
-app.get( "/", ( req, res ) => {
-    res.send( "soon™" );
-} );
+const app = express() 
+const port = 8738
+const _dirname = __dirname.replace('\\dist', '')
+
+app.use(express.static(_dirname + '/static'))
+app.get('/', ( req, res ) => {
+	res.sendFile(_dirname + '/static/index.html')
+})
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+	console.log( `server started at http://localhost:${ port }` )
+})
