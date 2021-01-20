026720d065533dd225aa511e309321abdac97ab8
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-01-19 20:18:49 -0700

    some pog stuff

--- src/main.ts
+++ src/main.ts
@@ -1,109 +1,17 @@
 import express from 'express'
-import fs from 'fs'
-import {promisify} from 'util'
-import {extname} from 'path'
-import moment from 'moment'
-import formidable from 'express-formidable'
-import { RateLimiterMemory } from 'rate-limiter-flexible'
 import session from 'express-session'
-import { v5 as uuidV5, v4 as uuidV4 } from 'uuid'
-import ejs from 'ejs'
-import glob from 'glob'
+import {v4 as uuidV4, v5 as uuidV5} from 'uuid'
+import {
+	_dirname, allowedUsers,	delFile,
+	formParse, handleUpload, readFile,
+	adminLocked, rateLimit, checkFiles,
+	exists, readDir
+} from './utilities'
 
 const app = express()
-app.set('view engine', 'ejs')
 const port = 8738
-const _dirname = __dirname.replace(/[\\/]dist/, '')
-const stat = promisify(fs.stat)
-const readDir = promisify(fs.readdir)
-const readFile = promisify(fs.readFile)
-const delFile = promisify(fs.unlink)
-const renameFile = promisify(fs.rename)
-const formParse = () => formidable({
-	uploadDir: _dirname + '/files',
-	keepExtensions: true
-})
-const randID = () => {
-	const chars = [...'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890']
-	let str = ''
-	for (let i = 0;i<10;i++) {
-		str += chars[Math.floor(Math.random() * chars.length)]
-	}
-	return str
-}
-interface apiResponse {
-	success: boolean,
-	reason?: string,
-	url?: string
-}
-interface allowedUsers {
-	'upload': {
-		[s: string]: string
-	},
-	'admin': {
-		[s: string]: string
-	}
-}
-const adminLocked = (req, res, next) => {
-	if (req.session['admin']) {
-		next()
-	} else {
-		res.sendStatus(403)
-	}
-}
-const handleUpload = async (req): Promise<{ res: apiResponse, code: number }> => {
-	const file = req.files.file as File
-	if (!file || !req.fields.key) {
-		await delFile(file.path)
-		return {
-			res: {
-				success: false,
-				reason:	'File or key not given'
-			},
-			code: 422
-		}
-	}
-	const users: allowedUsers = JSON.parse((await readFile(_dirname + '/allowed-users.json')).toString())
-	if (!users.upload[req.fields.key]) {
-		await delFile(file.path)
-		return {
-			res: {
-				success: false,
-				reason: 'Invalid key'
-			},
-			code: 403
-		}
-	}
-	const id = randID()
-	const newName = id + extname(file.path)
-	const newPath = _dirname + '/files/' + newName
-	await renameFile(file.path, newPath)
-	return {
-		res: {
-			success: true,
-			url: `${req.protocol}://${req.get('host')}/${newName}`
-		},
-		code: 200
-	}
-}
 
-interface File {
-	size: number;
-	path: string;
-	name: string;
-	type: string;
-	lastModifiedDate?: Date;
-	hash?: string;
-
-	toJSON(): Record<string, unknown>;
-}
-
-// Two requests per second allowed
-const rateLimiter = new RateLimiterMemory({
-	points: 2,
-	duration: 60
-})
-
+app.set('view engine', 'ejs')
 app.use(session({
 	secret: uuidV5(uuidV4(), uuidV4()),
 	genid: () => uuidV5(uuidV4(), uuidV4()),
@@ -112,26 +20,9 @@ app.use(session({
 }))
 app.use(express.static(_dirname + '/static'))
 app.use(express.static(_dirname + '/files'))
-const rateLimit = async (req, res, next) => {
-	rateLimiter.consume(req.fields.key, 1)
-		.then(() => {
-			next()
-		})
-		.catch((rateLimiterRes) => {
-			res.set({
-				'Retry-After': rateLimiterRes.msBeforeNext / 1000,
-				'X-RateLimit-Limit': 2,
-				'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
-				'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
-			})
-			res.sendStatus(429)
-			const file = req.files.file as File
-			delFile(file.path)
-		})
-}
 
 app.get('/', ( req, res ) => {
-	res.sendFile(_dirname + '/static/index.html')
+	res.render('index')
 })
 
 app.post('/uploadfile', formParse(), rateLimit, async (req, res) => {
@@ -151,8 +42,29 @@ app.post('/login', formParse(), async (req, res) => {
 })
 
 app.get('/admin', adminLocked, async (req, res) => {
-	const files: string[] = await readDir(_dirname + '/files')
-	res.render('admin', { files })
+	await res.render('admin/index')
+})
+
+app.get('/admin/files', adminLocked, async (req, res) => {
+	await res.render('admin/files', {
+		files: await readDir(_dirname + '/files')
+	})
+})
+
+app.get('/admin/users', adminLocked, async (req, res) => {
+	const users: allowedUsers = JSON.parse((await readFile(_dirname + '/allowed-users.json')).toString())
+	await res.render('admin/users', {
+		users
+	})
+})
+
+app.get('/admin/*', adminLocked, async (req, res) => {
+	const path: string = req.path.replace(/^\//, '')
+	if (await exists(app.get('views') + '/' + path + '.ejs')) {
+		res.render(path)
+	} else {
+		res.sendStatus(404)
+	}
 })
 
 app.delete('/admin/files', adminLocked, formParse(), async (req, res) => {
@@ -166,25 +78,14 @@ app.delete('/admin/files', adminLocked, formParse(), async (req, res) => {
 })
 
 app.get('*', async (req, res) => {
-	try {
-		await stat(_dirname + '/static' + req.path + '.html')
-		res.sendFile(_dirname + '/static' + req.path + '.html')
-	} catch {
+	const path: string = req.path.replace(/^\//, '')
+	if (await exists(app.get('views') + '/' + path + '.ejs')) {
+		res.render(path)
+	} else {
 		res.sendStatus(404)
 	}
 })
 
-const checkFiles = async () => {
-	const files: string[] = await readDir(_dirname + '/files')
-	const statFiles = await Promise.all(files.map(f => stat(_dirname + '/files/' + f)))
-	const fileObj = Object.fromEntries(files.map((_, i) => [files[i], statFiles[i]]))
-	for (const file of Object.keys(fileObj)) {
-		const mtime = moment(fileObj[file].mtime)
-		if (moment.duration(mtime.diff(moment.now())).asMonths() >= 1) {
-			await delFile(_dirname + '/files/' + file)
-		}
-	}
-}
 // start the Express server
 app.listen(port, async () => {
 	console.log(`Server started on port ${port}`)
---
+++ src/utilities.ts
@@ -0,0 +1,161 @@
+import formidable from 'express-formidable'
+import {extname} from 'path'
+import {promisify} from 'util'
+import * as fs from 'fs'
+import {RateLimiterMemory} from 'rate-limiter-flexible'
+import moment from 'moment'
+import express, {Express} from 'express'
+
+export interface File {
+	size: number;
+	path: string;
+	name: string;
+	type: string;
+	lastModifiedDate?: Date;
+	hash?: string;
+
+	toJSON(): Record<string, unknown>;
+}
+
+export interface apiResponse {
+	success: boolean,
+	reason?: string,
+	url?: string
+}
+
+export interface allowedUsers {
+	'upload': {
+		[s: string]: string
+	},
+	'admin': {
+		[s: string]: string
+	}
+}
+
+export const _dirname = __dirname.replace(/[\\/]dist/, '')
+export const stat = promisify(fs.stat)
+export const readDir = promisify(fs.readdir)
+export const readFile = promisify(fs.readFile)
+// export const writeFile = promisify(fs.writeFile)
+export const delFile = promisify(fs.unlink)
+export const renameFile = promisify(fs.rename)
+export const exists = promisify(fs.exists)
+
+export const formParse = () => formidable({
+	uploadDir: _dirname + '/files',
+	keepExtensions: true
+})
+export const rateLimiter = new RateLimiterMemory({
+	points: 2,
+	duration: 60
+})
+
+export const rateLimit = async (req, res, next): Promise<void> => {
+	rateLimiter.consume(req.fields.key, 1)
+		.then(() => {
+			next()
+		})
+		.catch((rateLimiterRes) => {
+			res.set({
+				'Retry-After': rateLimiterRes.msBeforeNext / 1000,
+				'X-RateLimit-Limit': 2,
+				'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
+				'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+			})
+			res.sendStatus(429)
+			const file = req.files.file as File
+			delFile(file.path)
+		})
+}
+
+export const handleUpload = async (req): Promise<{ res: apiResponse, code: number }> => {
+	const file = req.files.file as File
+	if (!file || !req.fields.key) {
+		await delFile(file.path)
+		return {
+			res: {
+				success: false,
+				reason:	'File or key not given'
+			},
+			code: 422
+		}
+	}
+	const users: allowedUsers = JSON.parse((await readFile(_dirname + '/allowed-users.json')).toString())
+	if (!users.upload[req.fields.key]) {
+		await delFile(file.path)
+		return {
+			res: {
+				success: false,
+				reason: 'Invalid key'
+			},
+			code: 403
+		}
+	}
+	const id = randID()
+	const newName = id + extname(file.path)
+	const newPath = _dirname + '/files/' + newName
+	await renameFile(file.path, newPath)
+	return {
+		res: {
+			success: true,
+			url: `${req.protocol}://${req.get('host')}/${newName}`
+		},
+		code: 200
+	}
+}
+
+export const randID = () => {
+	const chars = [...'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890']
+	let str = ''
+	for (let i = 0;i<10;i++) {
+		str += chars[Math.floor(Math.random() * chars.length)]
+	}
+	return str
+}
+
+export const adminLocked = (req, res, next) => {
+	if (req.session['admin']) {
+		next()
+	} else {
+		res.sendStatus(403)
+	}
+}
+
+export const checkFiles = async () => {
+	const files: string[] = await readDir(_dirname + '/files')
+	const statFiles = await Promise.all(files.map(f => stat(_dirname + '/files/' + f)))
+	const fileObj = Object.fromEntries(files.map((_, i) => [files[i], statFiles[i]]))
+	for (const file of Object.keys(fileObj)) {
+		const mtime = moment(fileObj[file].mtime)
+		if (moment.duration(mtime.diff(moment.now())).asMonths() >= 1) {
+			await delFile(_dirname + '/files/' + file)
+		}
+	}
+}
+
+// export class JsonDB {
+// 	public path: string
+// 	constructor(file: string) {
+// 		const path = this.path = _dirname + '/' + file
+// 		if (fs.existsSync(path)) {
+// 			this.path = path
+// 		} else {
+// 			throw new Error('Specified db is not a file!')
+// 		}
+// 	}
+// 	private getFileContents = async () => {
+// 		return (await readFile(this.path)).toString()
+// 	}
+// 	private setFileContents = async (contents: string) => {
+// 		return (await writeFile(this.path, contents))
+// 	}
+// 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// 	public getContent = async (): Promise<Record<string, any>> => {
+// 		return JSON.parse(await this.getFileContents())
+// 	}
+//
+// 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// 	public setContent = async (data: Record<string, any>): Promise<void> => {
+// 		return JSON.stringify(await this.setFileContents(data))
+// 	}
+// }
